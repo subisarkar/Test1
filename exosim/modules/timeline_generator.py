8d73564cd47b21c1a7cacd17f04b781063705ba3
subisarkar <subhajit.sarkar@astro.cf.ac.uk>
2015-03-11 12:55:10 +0000

    timeline_generator module

---
+++ exosim/modules/timeline_generator.py
@@ -0,0 +1,20 @@
+#Time line generator module
+"""
+Created on Wed Mar 11 12:51:06 2015
+
+@author: Subi
+"""
+
+import numpy as np
+import scipy
+
+
+
+
+jitter = exosim.lib.jitter()
+
+
+
+    
+    
+    
